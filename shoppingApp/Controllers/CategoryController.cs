e7cc92279ab5f069bcc9b5c5488288bf472589f3
Utkarsh Tripathi <shandilya0103@gmail.com>
2025-05-29 15:23:28 +0530

    Add project files.

---
+++ shoppingApp/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShoppingApp.Data;
+using ShoppingApp.Models;
+
+namespace ShoppingApp.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db) //Since it's mentioned in program.cs then we can call it here as argument
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+          List<Category>objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+    }
+}
---
+++ shoppingApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace shoppingApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
