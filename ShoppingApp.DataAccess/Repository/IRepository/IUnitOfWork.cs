e7f0bdd444b4893038f9ff90f698b0c2b7e2588d
Utkarsh Tripathi <shandilya0103@gmail.com>
2025-06-05 18:58:00 +0530

    Made Controller and Views for product and completed CRUD

--- ShoppingApp.DataAccess/Repository/IRepository/IUnitOfWork.cs
+++ ShoppingApp.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -9,6 +9,7 @@ namespace ShoppingApp.DataAccess.Repository.IRepository
     public interface IUnitOfWork
     {
         ICategoryRepository Category { get; }
+        IProductRepository Product { get; }
 
         void Save(); 
     }
