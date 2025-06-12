3495704823221040bddda52e2949623c8ea0c504
Utkarsh Tripathi <shandilya0103@gmail.com>
2025-06-12 15:22:26 +0530

    Added Shopping cart repository and in Unit Of Work

---
+++ ShoppingApp.DataAccess/Repository/ApplicationUserRepository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShoppingApp.DataAccess.Data;
+using ShoppingApp.DataAccess.Repository.IRepository;
+using ShoppingApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingApp.DataAccess.Repository
+{
+    public class ApplicationUserRepository : Repository<Category>, ICategoryRepository
+    {
+        private ApplicationDbContext _db;
+
+        public ApplicationUserRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+    }
+}
---
+++ ShoppingApp.DataAccess/Repository/ShoppingCartRepository.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShoppingApp.DataAccess.Data;
+using ShoppingApp.DataAccess.Repository.IRepository;
+using ShoppingApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingApp.DataAccess.Repository
+{
+    public class ShoppingCartRepository : Repository<ShoppingCart>, IShoppingCartRepository
+    {
+        private ApplicationDbContext _db;
+
+        public ShoppingCartRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+      
+        public new void Update(ShoppingCart obj) //Keep this update in mind
+        {
+            _db.ShoppingCarts.Update(obj);
+        }
+    }
+}
