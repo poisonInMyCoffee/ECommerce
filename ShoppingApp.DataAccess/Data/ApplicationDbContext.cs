e6e1175e2348b3ee74ce78d122ec7944fc98eb8c
Utkarsh Tripathi <shandilya0103@gmail.com>
2025-06-13 14:43:53 +0530

    Made ORderDetails and orderHeader model and dbcontext and added migration

--- ShoppingApp.DataAccess/Data/ApplicationDbContext.cs
+++ ShoppingApp.DataAccess/Data/ApplicationDbContext.cs
@@ -15,6 +15,8 @@ namespace ShoppingApp.DataAccess.Data
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
         public DbSet<Company> Companies { get; set; }
         public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<OrderHeader> OrderHeaders { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
--- shoppingApp/Areas/Customer/Controllers/CartController.cs
+++ shoppingApp/Areas/Customer/Controllers/CartController.cs
@@ -41,6 +41,10 @@ namespace ShoppingApp.Areas.Customer.Controllers
             return View(ShoppingCartVM);
         }
 
+        public IActionResult Summary()
+        {
+            return View();
+        }
 
         public IActionResult Plus(int cartId)
         {
