19bb464fb56a0f4ba6ace91b3507ba883417e4cb
Utkarsh Tripathi <shandilya0103@gmail.com>
2025-06-19 18:39:05 +0530

    Made view component for cart and the view file too

---
+++ shoppingApp/ViewComponents/ShoppingCartViewComponent.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ShoppingApp.DataAccess.Repository.IRepository;
+using ShoppingApp.Utilities;
+using System.Security.Claims;
+
+namespace ShoppingApp.ViewComponents
+{
+    public class ShoppingCartViewComponent: ViewComponent
+    {
+        IUnitOfWork _unitOfWork;
+        public ShoppingCartViewComponent(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim=claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            if (claim != null)
+            {
+                if (HttpContext.Session.GetInt32(SD.SessionCart) == null)
+                {
+                    HttpContext.Session.SetInt32(SD.SessionCart,
+                   _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == claim.Value).Count());
+                }
+                return View(HttpContext.Session.GetInt32(SD.SessionCart));
+            }
+            else
+            {
+                HttpContext.Session.Clear();
+                return View(0);
+            }
+        }
+    }
+}
