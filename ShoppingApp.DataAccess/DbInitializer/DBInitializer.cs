7a3c5bc5f78dcfb5bbfb7c03058e2c3ac02b08d4
Utkarsh Tripathi <shandilya0103@gmail.com>
2025-06-20 16:17:11 +0530

    DbInitializer

---
+++ ShoppingApp.DataAccess/DbInitializer/DBInitializer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingApp.DataAccess.DbInitializer
+{
+    public class DBInitializer : IDbInitializer
+    {
+        public void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
